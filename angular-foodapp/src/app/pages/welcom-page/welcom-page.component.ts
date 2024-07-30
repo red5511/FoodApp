458eba4c2395c507a71ac875c26d206432210901
red55 <xd@>
2024-07-30 23:31:20 +0200

    angular stuff

---
+++ angular-foodapp/src/app/pages/welcom-page/welcom-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcom-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcom-page.component.html',
+  styleUrl: './welcom-page.component.scss'
+})
+export class WelcomPageComponent {
+
+}
