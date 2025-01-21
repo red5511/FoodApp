bf5a42042d9da57fdcdeb56444ca87b43f4fcb17
red5511 <maciekfranczak@onet.eu>
2025-01-21 02:08:41 +0100

    product page part2

--- src/main/java/com/foodapp/foodapp/product/ProductDto.java
+++ src/main/java/com/foodapp/foodapp/product/ProductDto.java
@@ -1,6 +1,5 @@
 package com.foodapp.foodapp.product;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,13 +8,9 @@ import java.math.BigDecimal;
 @Builder
 @Getter
 public class ProductDto {
-    @Schema(required = true)
     private Long id;
-    @Schema(required = true)
     private Long companyId;
-    @Schema(required = true)
     private String name;
-    @Schema(required = true)
     private BigDecimal price;
     private String imgUrl;
     private String description;
